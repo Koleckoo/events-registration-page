58b7c35c8155eded67307a792b9eda6ec6086f6a
Jenda <jan.illetsko@gmail.com>
2023-02-03 13:09:33 +0100

    overlay and modul

--- src/js/components/Event.js
+++ src/js/components/Event.js
@@ -4,17 +4,48 @@ export class Event {
         this.element.classList.add("events__container");
         this.id = properties.id
 
+        this.displayModal = false;
+        this.overlayOn = false;
+
         this.name = properties.name
 
         this.createNewEvent();
     }
-
+    
     createNewEvent = () => {
         this.element.innerHTML = `
-            <div class="event">
-                ${this.name}
-                <button id="${this.id}" class="event__btn-more">More</button>
-            </div>
+        <div class="event">
+        ${this.name}
+        <button id="${this.id}" class="event__btn-more">More</button>
+        <div id="myModal" class="event__modal">
+        <div class="overlay"></div>
+        <div class="event__modal-content"></div>
+        </div>
+
+        </div>
         `
+        const moreModal = this.element.querySelector("#myModal");
+        if (this.displayModal) {
+            moreModal.style.display = "block";
+
+        }
+
+        const overlayDiv = document.createElement("div");
+        document.body.appendChild(overlayDiv);
+        if (this.overlayOn) {
+            overlayBody.classList.add("overlay");
+        }
+        this.addListenertoButton();
     }
+
+    addListenertoButton = () => {
+        const moreButton = this.element.querySelector(".event__btn-more");
+        
+        moreButton.addEventListener(("click"), () => {
+            this.displayModal = !this.displayModal;
+            this.overlayOn = !this.overlayOn;
+            this.createNewEvent();
+        })
+    }
+
 }
